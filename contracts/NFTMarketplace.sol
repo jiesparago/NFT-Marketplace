eece8af8ddfe9e98c37dd4e0a399f1ff417a1d2d
Sep <esparago.joseph0002@gmail.com>
2023-01-16 22:43:42 +0800

    first commit

---
+++ contracts/NFTMarketplace.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+//Internal Import for NFT openzipline
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import "hardhat/console.sol";
+
+contract NFTMarketplace is ERC721URIStorage {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _itmesSold;
+
+    uint256 listingPrice = 0.0025 ether;
+
+    address payable owner;
+
+    mapping(uint256 => MarketItem) private idMarketItem;
+
+    struct MarketItem {
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool sold;
+    }
+
+    event idMarketItemCreated(
+        uint256 indexed tokenId,
+        address seller,
+        address owner,
+        uint256 price,
+        bool sold
+    );
+
+    modifier onlyOwner() {
+        require(
+            msg.sender == owner,
+            "only owner of the marketplace can change the listing price"
+        );
+        _;
+    }
+
+    constructor() ERC721("NFT Metaverse TOken", "MYNFT") {
+        owner == payable(msg.sender);
+    }
+
+    function updateListingPrice(uint256 _listingPrice)
+        public
+        payable
+        onlyOwner
+    {}
+
+    function getListingPrice() public view returns (uint256) {
+        return listingPrice;
+    }
+
+    //Let create "Create NFT token function"
+
+    function createToken(string memory tokenURI, uint256 price)
+        public
+        payable
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newTokenId = _tokenIds.current();
+
+        _mint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+
+        createMarketItem()
+    }
+}
